99f7dbab3f4c8637dfc8b8122814a4d135bbeb2f
adam-gatherer <adam@gatherer.tech>
2023-08-16 14:48:25 +0000

    no idea where I'm at with this, giving up for today

--- sunset_app/main.py
+++ sunset_app/main.py
@@ -23,7 +23,7 @@ def sunset_from_lonlat(lonlat: tuple, today) -> dict:
 
     URL = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}"
     URL += f"&hourly=temperature_2m,precipitation_probability,visibility,windspeed_10m,soil_moisture_0_1cm"
-    URL += f"&daily=sunset&timezone=GMT&forecast_days=14"
+    URL += f"&daily=sunset&timezone=GMT&forecast_days=2"
     r = requests.get(url=URL)
     data = r.json()
     #sunset = (data['results']['sunset'])
@@ -39,8 +39,24 @@ def sunset_from_lonlat(lonlat: tuple, today) -> dict:
         #sunset = f"{data['daily']['sunset'][i]}"
         i += 1
     for key, val in fortnight.items():
-        #key = key.replace("T", "")
-        datetime_object = datetime.strptime(key, "%Y-%m-%d%H:%M")
+        date = datetime.strptime(key, "%Y-%m-%d%H:%M")
+        x = 0
+        for time in (data['hourly']['time']):
+            time = time.replace("T", "")
+            time = datetime.strptime(time, "%Y-%m-%d%H:%M")
+            if (time.day == date.day) and (time.hour in range ((date.hour) -2, (date.hour +1))):
+                fortnight[key] = (
+                    str(date),
+                    data['hourly']['temperature_2m'][x],
+                    data['hourly']['precipitation_probability'][x],
+                    data['hourly']['visibility'][x],
+                    data['hourly']['windspeed_10m'][x],
+                    data['hourly']['soil_moisture_0_1cm'][x]
+                )       
+            x += 1
+
+        for key, val in fortnight.items():
+            print(f'{key} - {val}')
         
     #for item in (data['hourly']):
     #   print(item[0])
@@ -51,3 +67,13 @@ today = (date.today())
 
 sunset_from_lonlat(lonlat, today)
 
+#time1 = "2023-08-16T19:48"
+#time1 = time1.replace("T", "")
+#time1 = datetime.strptime(time1, "%Y-%m-%d%H:%M")
+#timelist = ["2023-08-16T16:00", "2023-08-16T17:00", "2023-08-16T18:00", "2023-08-16T19:00", "2023-08-16T20:00", "2023-08-16T21:00", "2023-08-16T22:00"]
+
+#for time in timelist:
+#    time = time.replace("T", "")
+#    time = datetime.strptime(time, "%Y-%m-%d%H:%M")
+#    if time.hour in range((time1.hour)-2,time1.hour+1) :
+#        print(time)    
