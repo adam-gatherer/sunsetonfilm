1d0933d8dd068adb340983a90422a165aacc92ca
adam-gatherer <adam@gatherer.tech>
2023-08-23 10:11:40 +0000

    Finished first version of sunser_from_lonlat function, added find_good_dates function

--- sunset_app/main.py
+++ sunset_app/main.py
@@ -18,33 +18,32 @@ def sunset_from_lonlat(lonlat: tuple, today) -> dict:
     lat = lonlat[1]
     URL = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}"
     URL += f"&hourly=temperature_2m,precipitation_probability,visibility,windspeed_10m,windspeed_80m,soil_moisture_0_1cm"
-    URL += f"&daily=sunset&timezone=GMT&forecast_days=2"
+    URL += f"&daily=sunset&timezone=GMT&forecast_days=14"
     r = requests.get(url=URL)
     data = r.json()
-    i = 0
+    sunset_list_index = 0
     fortnight_dict = {}
-   
     for time in (data['daily']['time']):
-        sunsetlist = (data['daily']['sunset'])
-        sunset_datetime = datetime.fromisoformat(sunsetlist[i])
+        sunset_list = (data['daily']['sunset'])
+        sunset_datetime = datetime.fromisoformat(sunset_list[sunset_list_index])
         fortnight_dict[sunset_datetime] = ""
 
+        #
+        # Gets the hours around sunset
+        #
         hours_datetime = [datetime.fromisoformat(hour) for hour in (data['hourly']['time'])]
-
         lower_limit = sunset_datetime - timedelta(hours=2)
         upper_limit = sunset_datetime + timedelta(hours=1)
-
-        print(sunset_datetime)
-
         indexes_of_interest = [
-            i for i, hour in enumerate(hours_datetime)
+            h for h, hour in enumerate(hours_datetime)
             if hour > lower_limit and hour < upper_limit
         ]
 
-       
+        #
+        # Creates list of weather data for each hour beore sunset
+        #
         weather_list = [
-            (data['hourly']['time'][i],
-            data['hourly']['temperature_2m'][i],
+            (data['hourly']['temperature_2m'][i],
             data['hourly']['precipitation_probability'][i],
             data['hourly']['visibility'][i],
             data['hourly']['windspeed_10m'][i],
@@ -52,17 +51,8 @@ def sunset_from_lonlat(lonlat: tuple, today) -> dict:
             data['hourly']['soil_moisture_0_1cm'][i])
             for i in indexes_of_interest
         ]
-
         
-        #avg_temp = 0
-        #avg_precip = 0
-        #avg_vis = 0
-        #avg_wind10 = 0
-        #avg_wind80 = 0
-        #avg_moist = 0
-
         data_list = [
-            data['hourly']['time'][i],
             avg_temp := 0,
             avg_precip := 0,
             avg_vis := 0,
@@ -70,29 +60,41 @@ def sunset_from_lonlat(lonlat: tuple, today) -> dict:
             avg_wind80 := 0,
             avg_moist := 0
         ]
-        
-        x = 1
-        d = 0
-        for weather in weather_list:
-            #data_list[0] += weather[1]
-            #data_list[1] += weather[2]
-            #data_list[2] += weather[3]
-            #data_list[3] += weather[4]
-            #data_list[4] += weather[5]
-            #data_list[5] += weather[6]
-            for c in range(1, 7):
-                data_list[c] += weather[c]
-                #data_list[c] = round(data_list[c], 2)
 
+        #
+        # Get the avg weather data for the hours before sunset
+        #
+        for weather in weather_list:
+            for c in range(0, 6):
+                try:
+                    data_list[c] += weather[c]
+                except:
+                    None
+        avg_data_list = []
         for data_entry in data_list:
             if type(data_entry) == float:
-                data_entry = data_entry / len(weather_list)
+                no_of_entries = len(weather_list)
+                data_entry = round((data_entry / no_of_entries), 2)
+            avg_data_list.append(data_entry)
+
+        
+
+        
+        fortnight_dict[sunset_datetime] = avg_data_list
+        sunset_list_index += 1
+    
+    return fortnight_dict
 
 
-        print(data_list)
-
+def find_good_dates(fortnight_dict: dict):
+    for key, val in fortnight_dict.items():
+        if (val[0] > 10) and (val[1] < 80):
+            print(key)
+    
 
 
 lonlat = (-3.251163, 55.977057)
 today = (date.today())
-sunset_from_lonlat(lonlat, today)
+fortnight_dict = sunset_from_lonlat(lonlat, today)
+
+#find_good_dates(fortnight_dict)
